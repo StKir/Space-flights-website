72749b6d886cc955443a67a70d072160a3e01f0f
Kirill Strukov <kirillstrukov2@gmail.com>
2021-12-03 15:56:21 +0300

    6section

---
+++ js/mainscr.js
@@ -0,0 +1,44 @@
+// Slider
+const sliderImgs = document.querySelectorAll('.slider-list'),
+      sliderWrp = document.querySelector('.section-slider__slider--img'),
+      dots = document.querySelectorAll('.dot');
+
+      let index = 0;
+const activeSlide = (n) =>{
+    for(slide of sliderImgs){
+        slide.classList.remove('slider-list--active', 'fade' )
+    }
+    sliderImgs[n].classList.add('slider-list--active', 'fade');
+}   
+const activeDots = (n) =>{
+    for(dot of dots){
+        dot.classList.remove('active_dot')
+    }
+    dots[n].classList.add('active_dot');
+}   
+const nextSlide = ()=>{
+    if(index == sliderImgs.length -1 ){
+        index = 0;
+        activeSlide(index);
+        activeDots(index)
+    }else{
+        index++;
+        activeSlide(index);
+        activeDots(index);
+    }
+}
+sliderWrp.addEventListener('click', nextSlide);
+dots.forEach((item, indexDot) => {
+    item.addEventListener('click', ()=>{
+        clearInterval(sliderInterval);
+        setInterval(nextSlide, 5000);
+        index = indexDot;
+        console.log(indexDot);
+        for(dot of dots){
+            dot.classList.remove('active_dot');
+        }
+        item.classList.add('active_dot');
+        activeSlide(index);
+    })
+})
+let sliderInterval = setInterval(nextSlide, 2500);
