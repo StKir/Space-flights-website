2364f98535b1a7f1d06b4ba018faca9510884c37
Kirill Strukov <kirillstrukov2@gmail.com>
2021-12-22 18:50:48 +0300

    adapt up

---
+++ js/preloader.js
@@ -0,0 +1,19 @@
+let mask = document.querySelector('.mask');
+window.addEventListener('load', ()=>{
+    console.log('hide');
+    mask.classList.add('hide-preloader');
+    mask.remove();
+})
+
+//burger-menu
+
+const openMobileMenu = document.querySelector('.burger-nav');
+const burgerMenuOverlay = document.querySelector('.menu-overlay');
+const closeMobileMenu = document.querySelector('.burger-nav-close');
+
+openMobileMenu.addEventListener('click', ()=>{
+    burgerMenuOverlay.classList.add('menu-overlay--active', 'fade-menu');
+})
+closeMobileMenu.addEventListener('click', ()=>{
+    burgerMenuOverlay.classList.remove('menu-overlay--active', 'fade-menu');
+})
